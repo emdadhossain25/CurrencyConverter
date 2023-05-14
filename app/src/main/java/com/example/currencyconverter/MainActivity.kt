52a5e23c10bb55556e2ddbfdb41029007bc3b7a0
emdad <emdad@ergo-ventures.com>
2023-05-14 11:32:43 +0600

    added library for compose and retrofit

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.currencyconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import com.example.currencyconverter.home.HomeScreen
+import com.example.currencyconverter.ui.theme.CurrencyConverterTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CurrencyConverterTheme {
+                CurrencyConverterAppTheme()
+            }
+        }
+    }
+}
+
+@Composable
+fun CurrencyConverterAppTheme() {
+    HomeScreen()
+}
---
+++ app/src/main/java/com/example/currencyconverter/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.currencyconverter.home
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("Hello World!")
+}
