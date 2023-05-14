92530d877c81f86955bc037588a475bfed22e048
emdad <emdad@ergo-ventures.com>
2023-05-14 21:58:14 +0600

    fake data added also made the appmodule provide classes singleton

--- app/src/main/java/com/example/currencyconverter/categories/CategoriesScreen.kt
+++ app/src/main/java/com/example/currencyconverter/categories/CategoriesScreen.kt
@@ -1,11 +1,29 @@
 package com.example.currencyconverter.categories
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CategoriesScreen() {
-    Text(
-        text = "Initial Setup Done"
+    val currencies = getFakeData()
+    Column {
+        Text(text = "Initial Setup done")
+        Spacer(modifier = Modifier.height(30.dp))
+        currencies.forEach {
+            Text(text = it)
+        }
+    }
+}
+
+fun getFakeData(): List<String> {
+    return listOf(
+        "USD",
+        "GBP",
+        "JY"
     )
 }
